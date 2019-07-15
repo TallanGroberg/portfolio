79bdfd0485940ad64d410194733cf12674084e4c
tallan groberg <tallan.taven@gmail.com>
2019-07-15 09:33:27 -0600

    to switch branch

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Navbar = () => {
+
+
+    return (
+        <>
+        <NavStyle>
+        navbar
+        <hr />
+
+        </NavStyle>
+        
+
+        </>
+    )
+}
+
+export default Navbar;
+
+
+const NavStyle = styled.div `
+    background-color: 
+`;
